987089a2336de58df149bc80efe97278a06e0d6c
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-09-18 09:30:41 +0200

    Add Fish configuration

---
+++ fishfiles/config.fish
@@ -0,0 +1,62 @@
+set PATH ~/.local/bin $PATH
+
+# Remove the default Fish greeting
+set fish_greeting
+
+# Aliases
+function g --wraps git
+  git $argv;
+end
+
+function v
+  nvim $argv;
+end
+
+function vim
+  nvim $argv;
+end
+
+function gr
+  grep -rn $argv;
+end
+
+function cat
+  bat $argv;
+end
+
+function grc
+  git add .; and git rebase --continue;
+end
+
+# Ruby on Rails Aliases
+function b
+  bundle $argv;
+end
+
+function pewpew
+  bin/rails db:migrate db:test:prepare;
+end
+
+function spring
+  ./bin/spring;
+end
+
+function import_db
+  bin/rake db:drop db:create; and mysql aligni_development -u root -p < ~/aligni-db/latest; and bin/rake db:migrate db:test:prepare
+end
+
+function pewpew
+  bundle exec rake db:migrate db:test:prepare
+end
+
+function console
+  bin/rails console
+end
+
+function srv
+  bin/rails server -b 0.0.0.0
+end
+
+# Rbenv
+set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+status --is-interactive; and source (rbenv init -|psub)
