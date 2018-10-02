a6d76c03aebde5f5b099c3b4b268163e5bcd87f8
Nebojša Stričević <nebojsa.stricevic@gmail.com>
2018-10-02 08:11:34 +0200

    Fix SSH to localhost command

--- fishfiles/config.fish
+++ fishfiles/config.fish
@@ -31,7 +31,7 @@ end
 function vbox
   switch (count $argv)
   case 0
-    ssh -p 2222 -o ForwardX11=yes -o ForwardX11Trused=yes -o ForwardAgent=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/def/null strika@localhost
+    ssh -p 2222 -o ForwardX11=yes -o ForwardX11Trusted=yes -o ForwardAgent=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/def/null strika@localhost
   case 1
     ssh-add
     VBoxHeadless --startvm $argv[1] &
