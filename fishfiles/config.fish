f13b649b0a08a3397b2bb18c53acfdda95eebecf
Nebojša Stričević <strika@hackberry.dev>
2022-03-07 08:18:48 +0000

    Clean up aliases

--- fishfiles/config.fish
+++ fishfiles/config.fish
@@ -11,24 +11,21 @@ fundle init
 set fish_greeting
 
 # Aliases
+alias gr="grep -rn"
 alias e="kak"
 alias p="cd ~/wiki; alas --insert-days 3 --remove-empty-days --skip-backup --schedule-tasks scheduled.md plan.md; e plan.md"
 alias play="mplayer -subcp cp1250"
 
 # Git aliases
 alias g="git"
-alias gr="grep -rn"
 alias grc="git add .; and git rebase --continue"
 alias grm="git rebase -i master"
-alias gpsf="git push -f"
 
 # Ruby on Rails aliases
 alias b="bundle"
-alias pewpew="bin/rails db:migrate db:test:prepare"
-alias spring="./bin/spring"
-alias import_db="env DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rake db:drop db:create; and mysql aligni_development -u developer -p < ~/aligni-db/latest; and bin/rake db:migrate db:test:prepare"
-alias pewpew="bundle exec rake db:migrate db:test:prepare"
 alias console="bin/rails console"
+alias import_db="env DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rake db:drop db:create; and mysql aligni_development -u developer -p < ~/aligni-db/latest; and bin/rake db:migrate db:test:prepare"
+alias pewpew="bin/rails db:migrate db:test:prepare"
 alias srv="bin/rails server -b 0.0.0.0"
 
 function vbox
